e6fbd3684e824469a3ff6cc5ca2552849b79ecfc
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-06-28 14:18:04 -0300

    bug fix login

--- back-bate-papo-uol/src/index.js
+++ back-bate-papo-uol/src/index.js
@@ -13,19 +13,19 @@ server.listen(4000, () => console.log("Running server..."));
 
 // data persistence
 if (!fs.existsSync("./src/messages.json")) {
-    fs.writeFileSync("./src/messages.json", JSON.stringify([]));
+	fs.writeFileSync("./src/messages.json", JSON.stringify([]));
 }
 
 if (!fs.existsSync("./src/participants.json")) {
-    fs.writeFileSync("./src/participants.json", JSON.stringify([]));
+	fs.writeFileSync("./src/participants.json", JSON.stringify([]));
 }
 
 function updateParticipantsJson() {
-    fs.writeFileSync("./src/participants.json", JSON.stringify(participants, null, 2));
+	fs.writeFileSync("./src/participants.json", JSON.stringify(participants, null, 2));
 }
 
 function updateMessagesJson() {
-    fs.writeFileSync("./src/messages.json", JSON.stringify(messages, null, 2));
+	fs.writeFileSync("./src/messages.json", JSON.stringify(messages, null, 2));
 }
 
 let participants = JSON.parse(fs.readFileSync("./src/participants.json"));
@@ -36,148 +36,148 @@ const interval = setInterval(clearInactiveParticipants, 15000);
 
 // routes
 server.post("/participants", (req, res) => {
-    const newParticipant = req.body;
+	const newParticipant = req.body;
+    console.log(newParticipant.name);
+	newParticipant.name = stripHtmlAndTrim(newParticipant.name);
+	if (validWithJOI(validateUserSchema, newParticipant) || availableUsername(newParticipant)) {
+		res.sendStatus(400);
+		return;
+	}
 
-    newParticipant.name = stripHtmlAndTrim(newParticipant.name);
-    if (!validWithJOI(validateUserSchema, newParticipant) || !availableUsername(newParticipant)) {
-        res.sendStatus(400);
-        return;
-    }
+	newParticipant.lastStatus = Date.now();
+	participants.push(newParticipant);
 
-    newParticipant.lastStatus = Date.now();
-    participants.push(newParticipant);
+	const newMessage = {
+		from: newParticipant.name,
+		to: "Todos",
+		text: "entra na sala...",
+		type: "status",
+		time: dayjs(Date.now()).format("HH:mm:ss"),
+	};
+	messages.push(newMessage);
 
-    const newMessage = {
-        from: newParticipant.name,
-        to: "Todos",
-        text: "entra na sala...",
-        type: "status",
-        time: dayjs(Date.now()).format("HH:mm:ss"),
-    };
-    messages.push(newMessage);
-
-    res.sendStatus(200);
-    updateParticipantsJson();
-    console.log("New participant add...");
+	res.sendStatus(200);
+	updateParticipantsJson();
+	console.log("New participant add...");
 });
 
 server.get("/participants", (req, res) => {
-    res.send(participants);
-    console.log("Send participants list...");
+	res.send(participants);
+	console.log("Send participants list...");
 });
 
 server.get("/messages", (req, res) => {
-    const limitMessages = req.query.limit || messages.length;
-    const thisUser = stripHtmlAndTrim(req.headers.user);
-    const messagesFiltered = userMessagesFilter(thisUser).slice(-limitMessages);
+	const limitMessages = req.query.limit || messages.length;
+	const thisUser = stripHtmlAndTrim(req.headers.user);
+	const messagesFiltered = userMessagesFilter(thisUser).slice(-limitMessages);
 
-    res.status(200).send(messagesFiltered);
-    console.log("Send messages to front...");
+	res.status(200).send(messagesFiltered);
+	console.log("Send messages to front...");
 });
 
 server.post("/messages", (req, res) => {
-    const newMessage = sanitizeMessage(req.body);
-    newMessage.from = stripHtmlAndTrim(req.headers.user);
+	const newMessage = sanitizeMessage(req.body);
+	newMessage.from = stripHtmlAndTrim(req.headers.user);
 
-    if (!validNewMessage(newMessage)) {
-        res.sendStatus(400);
-        return;
-    }
+	if (!validNewMessage(newMessage)) {
+		res.sendStatus(400);
+		return;
+	}
 
-    newMessage.time = dayjs(Date.now()).format("HH:mm:ss");
-    messages.push(newMessage);
+	newMessage.time = dayjs(Date.now()).format("HH:mm:ss");
+	messages.push(newMessage);
 
-    res.sendStatus(200);
-    updateMessagesJson();
-    console.log("New message received");
+	res.sendStatus(200);
+	updateMessagesJson();
+	console.log("New message received");
 });
 
 server.post("/status", (req, res) => {
-    const thisUser = stripHtmlAndTrim(req.headers.user);
-    const thisParticipant = participants.find((p) => p.name === thisUser);
-    const index = participants.indexOf(thisParticipant);
+	const thisUser = stripHtmlAndTrim(req.headers.user);
+	const thisParticipant = participants.find((p) => p.name === thisUser);
+	const index = participants.indexOf(thisParticipant);
 
-    if (!thisParticipant) {
-        res.sendStatus(400);
-        return;
-    }
+	if (!thisParticipant) {
+		res.sendStatus(400);
+		return;
+	}
 
-    thisParticipant.lastStatus = Date.now();
-    participants.splice(index, 1, thisParticipant);
+	thisParticipant.lastStatus = Date.now();
+	participants.splice(index, 1, thisParticipant);
 
-    res.sendStatus(200);
-    updateParticipantsJson();
-    console.log("Participant timestamp att");
+	res.sendStatus(200);
+	updateParticipantsJson();
+	console.log("Participant timestamp att");
 });
 
 function userMessagesFilter(user) {
-    return messages.filter(
-        (message) =>
-            message.type === "message" ||
-            message.type === "status" ||
-            message.from === user ||
-            message.to === user ||
-            message.to === "Todos"
-    );
+	return messages.filter(
+		(message) =>
+			message.type === "message" ||
+			message.type === "status" ||
+			message.from === user ||
+			message.to === user ||
+			message.to === "Todos"
+	);
 }
 
 function clearInactiveParticipants() {
-    const now = Date.now();
-    const nowHour = dayjs(now).format("HH:mm:ss");
-    const removedParticipants = participants.filter((p) => now - p.lastStatus >= 10000);
+	const now = Date.now();
+	const nowHour = dayjs(now).format("HH:mm:ss");
+	const removedParticipants = participants.filter((p) => now - p.lastStatus >= 10000);
 
-    removedParticipants.forEach((rp) =>
-        messages.push({ from: rp.name, to: "Todos", text: "sai da sala...", type: "status", time: nowHour })
-    );
+	removedParticipants.forEach((rp) =>
+		messages.push({ from: rp.name, to: "Todos", text: "sai da sala...", type: "status", time: nowHour })
+	);
 
-    participants = participants.filter((p) => now - p.lastStatus <= 10000);
+	participants = participants.filter((p) => now - p.lastStatus <= 10000);
 
-    updateParticipantsJson();
-    console.log("InactiveParticipants removed...");
+	updateParticipantsJson();
+	console.log("InactiveParticipants removed...");
 }
 
 // sanitize with strip-html library
 function sanitizeMessage(message) {
-    message.to = stripHtmlAndTrim(message.to);
-    message.from = stripHtmlAndTrim(message.to);
-    message.type = stripHtmlAndTrim(message.type);
-    message.text = stripHtmlAndTrim(message.text);
-    return message;
+	message.to = stripHtmlAndTrim(message.to);
+	message.from = stripHtmlAndTrim(message.to);
+	message.type = stripHtmlAndTrim(message.type);
+	message.text = stripHtmlAndTrim(message.text);
+	return message;
 }
 
 // validate with Joi library schemas
 const validateUserSchema = Joi.object({
-    name: Joi.string().alphanum().min(3).required(),
+	name: Joi.string().alphanum().min(3).required(),
 });
 
 const validateNewMessageSchema = Joi.object({
-    to: Joi.string().alphanum().required(),
-    from: Joi.string().alphanum().required(),
-    text: Joi.string().alphanum().min(1).required(),
-    type: Joi.string().alphanum().required(),
+	to: Joi.string().alphanum().required(),
+	from: Joi.string().alphanum().required(),
+	text: Joi.string().alphanum().min(1).required(),
+	type: Joi.string().alphanum().required(),
 });
 
 function validNewMessage(message) {
-    if (!validWithJOI(validateNewMessageSchema, message)) {
-        return false;
-    }
-    if (!["message", "private_message"].includes(message.type)) {
-        return false;
-    }
-    if (!participants.find((p) => p.name === message.from)) {
-        return false;
-    }
-    return true;
+	if (validWithJOI(validateNewMessageSchema, message)) {
+		return false;
+	}
+	if (!["message", "private_message"].includes(message.type)) {
+		return false;
+	}
+	if (!participants.find((p) => p.name === message.from)) {
+		return false;
+	}
+	return true;
 }
 
 function stripHtmlAndTrim(text) {
-    return stripHtml(text).result.trim();
+	return stripHtml(text).result.trim();
 }
 
 function validWithJOI(schema, value) {
-    return schema.validate(value).error === undefined;
+	return !!schema.validate(value).error;
 }
 
 function availableUsername(participant) {
-    return participants.find((p) => p.name === participant.name);
+	return participants.find((p) => p.name === participant.name);
 }
